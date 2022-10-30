8969289af0833a8e20641bcc0235bf2b60316f54
Andrási Márton <jm09as@gmail.com>
2022-10-30 08:32:30 +0100

    hello world

---
+++ src/test/java/hu/ak_akademia/mss/MssApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.ak_akademia.mss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
