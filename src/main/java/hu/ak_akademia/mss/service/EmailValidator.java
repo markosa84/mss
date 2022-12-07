e14853ce658fe55393c61d3516a1fd148b96bc27
xy <tozsa.lajos@gmail.com>
2022-12-07 19:36:27 +0100

    EmailValidator upload

---
+++ src/main/java/hu/ak_akademia/mss/service/EmailValidator.java
@@ -0,0 +1,16 @@
+package hu.ak_akademia.mss.service;
+
+import hu.ak_akademia.mss.model.Client;
+import hu.ak_akademia.mss.service.exceptions.IncorrectEnteredDataException;
+
+
+public class EmailValidator {
+
+
+    public static void emailValidator(Client client) throws IncorrectEnteredDataException {
+        if (!client.getEmail().matches(".+@\\w+\\.[a-z]+")) {
+            throw new IncorrectEnteredDataException("Incorrect e-mail format!");
+        }
+    }
+
+}
