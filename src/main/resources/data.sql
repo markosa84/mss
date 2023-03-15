749374520439917c87808398ef52d61125292504
Andrási Márton <jm09as@gmail.com>
2023-03-15 20:09:01 +0100

    few small things changing and refactoring

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -17,7 +17,7 @@ INSERT INTO MSS_USER (DTYPE, ACTIVE,
    MOTHERS_NAME, GENDER,
   PHONE_NUMBER, ROLES)
   VALUES ('Doctor', 'false'
-  ,'doki@gmail.com', '21232f297a57a5a743894a0e4a801fc3', 'Csaba'
+  ,'doki@gmail.com', '1151371fe13913e1f31121181f51a311819311e1721881ea', 'Csaba'
   , 'Rác','1970-02-18', 'Szabadka',
   'Rajmund Ilona', (SELECT id FROM GENDER WHERE gender='male'),
    '+36706380258','ROLE_DOCTOR');
@@ -25,7 +25,7 @@ INSERT INTO MSS_USER (DTYPE, ACTIVE,
  INSERT INTO MSS_USER (DTYPE, ACTIVE,  EMAIL, PASSWORD, FIRST_NAME, LAST_NAME,
     DATE_OF_BIRTH, PLACE_OF_BIRTH, MOTHERS_NAME, GENDER,
      PHONE_NUMBER, ROLES)
-     VALUES ('Doctor', 'false', 'dokk@gmail.com', '21232f297a57a5a743894a0e4a801fc3', 'Zoli', 'Földi',
+     VALUES ('Doctor', 'false', 'dokk@gmail.com', '1151371fe13913e1f31121181f51a311819311e1721881ea', 'Zoli', 'Földi',
      '1967-09-18', 'Szeged', 'Rák Ilona',  (SELECT id FROM GENDER WHERE gender='male'),
       '+36306415258', 'ROLE_DOCTOR');
 
@@ -34,14 +34,14 @@ INSERT INTO MSS_USER (DTYPE, ACTIVE,
 INSERT INTO MSS_USER (DTYPE, ACTIVE, EMAIL, PASSWORD, FIRST_NAME, LAST_NAME,
  DATE_OF_BIRTH, PLACE_OF_BIRTH, MOTHERS_NAME, TAJNUMBER, GENDER,
   PHONE_NUMBER,REGISTRATION_DATE, ROLES)
-  VALUES ('Client','false', 'aleeeai@gmail.com', '21232f297a57a5a743894a0e4a801fc3', 'Árpi', 'Balasa',
+  VALUES ('Client','false', 'aleeeai@gmail.com', '1151371fe13913e1f31121181f51a311819311e1721881ea', 'Árpi', 'Balasa',
   '1970-02-18', 'Budapest', 'Pátrik Izolda', '12345118911', (SELECT id FROM GENDER WHERE gender='like_Lajos'),
    '+36706389858','2023-02-20 10:10:14','ROLE_CLIENT');
 
    INSERT INTO MSS_USER (DTYPE, ACTIVE, EMAIL, PASSWORD, FIRST_NAME, LAST_NAME,
        DATE_OF_BIRTH, PLACE_OF_BIRTH, MOTHERS_NAME, TAJNUMBER, GENDER,
         PHONE_NUMBER,REGISTRATION_DATE, ROLES)
-        VALUES ('Client', 'false', 'dottk@gmail.com', '21232f297a57a5a743894a0e4a801fc3', 'Zoli', 'Berkes',
+        VALUES ('Client', 'false', 'dottk@gmail.com', '1151371fe13913e1f31121181f51a311819311e1721881ea', 'Zoli', 'Berkes',
         '1967-01-18', 'Győr', 'Andula Erika', '11881256789', (SELECT id FROM GENDER WHERE gender='female'),
          '+36306410058',CURRENT_TIME,'ROLE_CLIENT');
 
@@ -56,7 +56,7 @@ INSERT INTO MSS_USER (DTYPE, ACTIVE,
    MOTHERS_NAME, GENDER,
   PHONE_NUMBER, ROLES)
   VALUES ('Assistant', 'false'
-  ,'assistant1@gmail.com', '21232f297a57a5a743894a0e4a801fc3', 'Erika'
+  ,'assistant1@gmail.com', '1151371fe13913e1f31121181f51a311819311e1721881ea', 'Erika'
   , 'Vörös','1978-06-28', 'Baja',
   'Andula Hajnalka', (SELECT id FROM GENDER WHERE gender='female'),
    '+36306382258','ROLE_ASSISTANT' );
@@ -69,7 +69,7 @@ INSERT INTO MSS_USER (DTYPE, ACTIVE,
    MOTHERS_NAME, GENDER,
   PHONE_NUMBER, ROLES)
   VALUES ('Admin', 'false'
-  ,'admin111@gmail.com', '21232f297a57a5a743894a0e4a801fc3', 'Csaba'
+  ,'admin111@gmail.com', '1151371fe13913e1f31121181f51a311819311e1721881ea', 'Csaba'
   , 'f0ldi','1970-02-18', 'Csantavér',
   'Horváth Ilona', (SELECT id FROM GENDER WHERE gender='male'),
    '+36706110258','ROLE_ADMIN' );
