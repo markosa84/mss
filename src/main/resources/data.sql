0f8f1cbf997d1575ca714e5944a0b8f94db0831a
inkeko <inkeko@gmail.com>
2023-04-17 20:32:08 +0200

    Data.sql file repair

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,7 +7,7 @@ VALUES (1,'Hungarian'), (2,'Deutsch'),(3, 'English'),(4, 'Chinese');
 ----gender data
 -----------------
 INSERT INTO Gender(gender_id,gender)
- VALUES (1,'Male'), (2,'Female'),(3, 'Other');
+ VALUES (1,'Male'), (2,'Female'),(3,'Other');
 
 ---- doktor data
 -----------------
@@ -19,14 +19,14 @@ INSERT INTO MSS_USER (DTYPE, ACTIVE,
  VALUES ('Doctor', 'false'
  ,'doki@gmail.com', '1151371fe13913e1f31121181f51a311819311e1721881ea', 'Csaba'
  , 'Rác','1970-02-18', 'Szabadka',
- 'Rajmund Ilona', (SELECT gender_id FROM GENDER WHERE gender='male'),
+ 'Rajmund Ilona', (SELECT gender_id FROM GENDER WHERE gender='Male'),
   '+36706380258','ROLE_DOCTOR');
 
  INSERT INTO MSS_USER (DTYPE, ACTIVE,  EMAIL, PASSWORD, FIRST_NAME, LAST_NAME,
    DATE_OF_BIRTH, PLACE_OF_BIRTH, MOTHERS_NAME, GENDER,
      PHONE_NUMBER, ROLES)
     VALUES ('Doctor', 'false', 'dokk@gmail.com', '1151371fe13913e1f31121181f51a311819311e1721881ea', 'Zoli', 'Földi',
-    '1967-09-18', 'Szeged', 'Rák Ilona',  (SELECT gender_id FROM GENDER WHERE gender='male'),
+    '1967-09-18', 'Szeged', 'Rák Ilona',  (SELECT gender_id FROM GENDER WHERE gender='Male'),
      '+36306415258', 'ROLE_DOCTOR');
 
 ----client data
@@ -35,14 +35,14 @@ INSERT INTO MSS_USER (DTYPE, ACTIVE, EMAIL, PASSWORD, FIRST_NAME, LAST_NAME,
  DATE_OF_BIRTH, PLACE_OF_BIRTH, MOTHERS_NAME, TAJNUMBER, GENDER,
   PHONE_NUMBER,REGISTRATION_DATE, ROLES)
   VALUES ('Client','false', 'aleeeai@gmail.com', '1151371fe13913e1f31121181f51a311819311e1721881ea', 'Árpi', 'Balasa',
-  '1970-02-18', 'Budapest', 'Pátrik Izolda', '12345118911', (SELECT gender_id FROM GENDER WHERE gender='male'),
+  '1970-02-18', 'Budapest', 'Pátrik Izolda', '12345118911', (SELECT gender_id FROM GENDER WHERE gender='Male'),
    '+36706389858','2023-02-20 10:10:14','ROLE_CLIENT');
 
    INSERT INTO MSS_USER (DTYPE, ACTIVE, EMAIL, PASSWORD, FIRST_NAME, LAST_NAME,
       DATE_OF_BIRTH, PLACE_OF_BIRTH, MOTHERS_NAME, TAJNUMBER, GENDER,
        PHONE_NUMBER,REGISTRATION_DATE, ROLES)
        VALUES ('Client', 'false', 'dottk@gmail.com', '1151371fe13913e1f31121181f51a311819311e1721881ea', 'Zoli', 'Berkes',
-       '1967-01-18', 'Győr', 'Andula Erika', '11881256789', (SELECT gender_id FROM GENDER WHERE gender='female'),
+       '1967-01-18', 'Győr', 'Andula Erika', '11881256789', (SELECT gender_id FROM GENDER WHERE gender='Female'),
          '+36306410058',CURRENT_TIME,'ROLE_CLIENT');
 
 --asssistant data
@@ -55,7 +55,7 @@ INSERT INTO MSS_USER (DTYPE, ACTIVE,
   VALUES ('Assistant', 'false'
   ,'assistant1@gmail.com', '1151371fe13913e1f31121181f51a311819311e1721881ea', 'Erika'
   , 'Vörös','1978-06-28', 'Baja',
-  'Andula Hajnalka', (SELECT gender_id FROM GENDER WHERE gender='female'),
+  'Andula Hajnalka', (SELECT gender_id FROM GENDER WHERE gender='Female'),
    '+36306382258','ROLE_ASSISTANT' );
 
 ----admin data
@@ -68,7 +68,7 @@ INSERT INTO MSS_USER (DTYPE, ACTIVE,
   VALUES ('Admin', 'false'
   ,'admin111@gmail.com', '1151371fe13913e1f31121181f51a311819311e1721881ea', 'Csaba'
   , 'Földi','1970-02-18', 'Csantavér',
-  'Horváth Ilona', (SELECT gender_id FROM GENDER WHERE gender='male'),
+  'Horváth Ilona', (SELECT gender_id FROM GENDER WHERE gender='Male'),
    '+36706110258','ROLE_ADMIN' );
 
 -----FinancialColleague
@@ -81,7 +81,7 @@ INSERT INTO MSS_USER (DTYPE, ACTIVE,
   VALUES ('FinancialColleague', 'false'
   ,'financ131@gmail.com', '1151371fe13913e1f31121181f51a311819311e1721881ea', 'Árpád'
   , 'Gyurita','1980-06-28', 'Nagyfény',
-  'Kovács Gyöngyi', (SELECT gender_id FROM GENDER WHERE gender='male'),
+  'Kovács Gyöngyi', (SELECT gender_id FROM GENDER WHERE gender='Male'),
    '+36306110228','ROLE_FINANCIALCOLLEAGUE' );
 
 
