5cb37c9a31afa1d20d8bc9b94541ee6398c7e78a
dexter <marton.fabry@gmail.com>
2023-07-09 11:08:57 +0200

    update email validator update phone number validator upload appointment screen views

--- src/main/java/hu/ak_akademia/mss/service/validators/EmailValidator.java
+++ src/main/java/hu/ak_akademia/mss/service/validators/EmailValidator.java
@@ -3,12 +3,12 @@ package hu.ak_akademia.mss.service.validators;
 import hu.ak_akademia.mss.service.Validator;
 import hu.ak_akademia.mss.service.exceptions.IncorrectEnteredDataException;
 
-
 class EmailValidator implements Validator<String> {
 
     @Override
     public void validate(String email) throws IncorrectEnteredDataException {
-        if (email == null || !email.matches(".+@\\w+\\.[a-z]+")) {
+        if (email == null || !email.matches("^([a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*" +
+                    "@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\\.)+[a-zA-Z]{2,7})$")) {
             throw new IncorrectEnteredDataException("emailError", "Incorrect! Correct form is e.g.: teszt@mail.hu");
         }
     }
--- src/main/java/hu/ak_akademia/mss/service/validators/PhoneNumberValidator.java
+++ src/main/java/hu/ak_akademia/mss/service/validators/PhoneNumberValidator.java
@@ -8,7 +8,7 @@ import hu.ak_akademia.mss.service.exceptions.IncorrectEnteredDataException;
 class PhoneNumberValidator implements Validator<String> {
     private static final String PHONE_NUMBER_REGEX = "^(06|\\+36)?" + //country code -> currently optional, delete ? to make it mandatory
             "[\\s\\-]?" + //delimiter (optional)
-            "(([2,3,7]0" + //HU mobile codes
+            "(([2,3,7,5]0" + //HU mobile codes - 20 30 50 70
             "[\\s\\-]?" +
             "\\d{3}[\\s\\-]?\\d{4})" + //HU mobile format
             "|((1|2[2-9]|3[1-7]|4[2,4-9]|5[1-7,9]|6[2,3,6,8,9]|7[1-9]|8[0,2-5,7-9]|9[2-6,9])" + //HU dialing codes
