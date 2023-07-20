261edd7d7ba29941fb9816f3da5f923989db6611
dexter <marton.fabry@gmail.com>
2023-07-20 15:32:34 +0200

    update MotherNameValidator update PhoneNumberValidator

--- src/main/java/hu/ak_akademia/mss/service/validators/MotherNameValidator.java
+++ src/main/java/hu/ak_akademia/mss/service/validators/MotherNameValidator.java
@@ -8,10 +8,14 @@ class MotherNameValidator implements Validator<String> {
 
     @Override
     public void validate(String mothersName) throws IncorrectEnteredDataException {
-        if (mothersName == null || !mothersName.equals("Ismeretlen") || !mothersName.matches("^(\\p{L}" + //must start with a letter
+        if ("Ismeretlen".equals(mothersName)) {
+            return;
+        }
+        if (mothersName == null || !mothersName.matches("^(\\p{L}" + //must start with a letter
                 "(?=\\S*\\s)" + //must contain at least one space
                 "[\\p{L}\\p{Mn}\\s'-.]{3,248}" +
-                "[\\p{L}.])$")) { //must end with a letter or a dot(for rare occasions when a suffix/abbrev. is used in a foreign maiden name like Jr.)
+                "[\\p{L}.])$") //must end with a letter or a dot(for rare occasions when a suffix/abbrev. is used in a foreign maiden name like Jr.)
+        ) {
             throw new IncorrectEnteredDataException("motherNameError", "The given name does not meet the formal requirements!");
         }
         MSSUserValidatorFactory.lengthValidation(mothersName);
