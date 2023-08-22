6481ff15e855eb2ec20c95e53b12a29c56b4b575
Andrási Márton <jm09as@gmail.com>
2023-08-22 21:57:37 +0200

    field vs value error solution

--- src/main/java/hu/ak_akademia/mss/controller/EditProfilController.java
+++ src/main/java/hu/ak_akademia/mss/controller/EditProfilController.java
@@ -2,16 +2,14 @@ package hu.ak_akademia.mss.controller;
 
 import hu.ak_akademia.mss.config.SessionMssUser;
 import hu.ak_akademia.mss.model.user.Client;
-import hu.ak_akademia.mss.model.user.Doctor;
 import hu.ak_akademia.mss.model.user.MssUser;
 import hu.ak_akademia.mss.service.RegistrationService;
-import hu.ak_akademia.mss.service.RegistrationVerificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.mail.MessagingException;
 import java.util.Map;
@@ -19,6 +17,7 @@ import java.util.Map;
 @Controller
 @RequestMapping("/")
 public class EditProfilController {
+
     @Autowired
     private SessionMssUser sessionMssUser;
 
@@ -48,27 +47,25 @@ public class EditProfilController {
 
     @GetMapping("/edit")
     public String edit_profil(Model model) {
-        Client user =(Client) sessionMssUser.getCurrentMssUser();
-        String passW ="";
-        model.addAttribute("passW", passW);
+        MssUser client = sessionMssUser.getCurrentMssUser();
+        model.addAttribute("passW", "alma");
         model.addAttribute("genderList", registrationService.getAllGender());
         model.addAttribute("languageList", registrationService.getLanguages());
         model.addAttribute("test", "körte");
-        model.addAttribute("client", user);
-
-
-
-
+        model.addAttribute("client", client);
         return "edit_profil";
     }
 
 
-
     @PostMapping("/saveData")
-    public String editRegistrationData(@RequestBody MultiValueMap<String, String> formData, Model model) throws MessagingException {
+    public String editRegistrationData(Client client, Model model) throws MessagingException {
+        Map<String, Object> map = model.asMap();
+        map.entrySet().forEach(System.out::println);
         //System.out.println(passW2 + " íme");
         //System.out.println(client);
-        System.out.println(formData);
+        System.out.println(client);
+        model.addAttribute("test", "test");
+        model.addAttribute("passW", "alma");
         /*
         Map<String, String> errorList = registrationService.testMSSUserData(client);
         if (errorList.isEmpty()) {
@@ -76,20 +73,12 @@ public class EditProfilController {
             registrationService.save(client);
             return "home";
         }
-
-
         model.addAttribute("passW", "");
         model.addAttribute("genderList", registrationService.getAllGender());
         model.addAttribute("languageList", registrationService.getLanguages());
         model.addAllAttributes(errorList);
-
          */
-        return "edit_profil";
-
+        return "redirect/edit";
     }
-
-
-
-
 
 }
