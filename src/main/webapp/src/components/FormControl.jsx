ad7d574d05907ed6769f8112f3b42029f311286f
Lenovo <inkeko@gmail.com>
2023-10-30 18:02:08 +0100

    update project.1

--- src/main/webapp/src/components/FormControl.jsx
+++
@@ -1,3 +0,0 @@
-export const FormControl = ({ children }) => {
-  return <div className="form-control">{children}</div>;
-};
--- src/main/webapp/src/components/footer.jsx
+++
@@ -1,7 +0,0 @@
-export const Footer = () => {
-  return (
-    <footer className="site-footer">
-      <p>This is going to be the footer of the whole page</p>
-    </footer>
-  );
-};
