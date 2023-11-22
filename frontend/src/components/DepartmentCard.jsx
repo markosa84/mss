5ab9db6e8cd01f4acd12cfb2bc3b975f1e9121d6
Balazs Braun <lucksmith.wd@gmail.com>
2023-11-22 14:51:10 +0800

    Updated DepartmentCard component: it displays default image if department image doesn't exist

--- frontend/src/components/DepartmentCard.jsx
+++ frontend/src/components/DepartmentCard.jsx
@@ -1,8 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export const DepartmentCard = ({ department, setSelectedDepartment }) => {
+  let [deptImgUrl, setDeptImgUrl] = useState();
+
+  useEffect(() => {
+    axios
+      .get(`/assets/dept-${department.name}.jpg`)
+      .then((res) => {
+        setDeptImgUrl(department.name);
+      })
+      .catch((err) => {
+        setDeptImgUrl("default");
+      });
+  }, []);
+
+  return (
+    <>
+      {deptImgUrl && (
+        <Card
+          department={department}
+          setSelectedDepartment={setSelectedDepartment}
+          deptImgUrl={deptImgUrl}
+        />
+      )}
+    </>
+  );
+};
+
+function Card({ department, setSelectedDepartment, deptImgUrl }) {
   return (
     <div className="card">
       <div className="card__image">
-        <img src={`/assets/dept-${department.name}.jpg`} alt="" />
+        <img src={`/assets/dept-${deptImgUrl}.jpg`} alt="" />
       </div>
       <div className="card__content">
         <div>
@@ -22,4 +52,4 @@ export const DepartmentCard = ({ department, setSelectedDepartment }) => {
       </div>
     </div>
   );
-};
+}
