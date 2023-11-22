66d567b24a4c5f9ca24d74650c03bd2a6a57d78c
Balazs Braun <lucksmith.wd@gmail.com>
2023-11-22 16:01:58 +0800

    Refactored DepartmentCard

--- frontend/src/components/DepartmentCard.jsx
+++ frontend/src/components/DepartmentCard.jsx
@@ -1,55 +1,55 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+
+const IMG_URL_BASE = "/assets/dept-";
 
 export const DepartmentCard = ({ department, setSelectedDepartment }) => {
-  let [deptImgUrl, setDeptImgUrl] = useState();
-
-  useEffect(() => {
-    axios
-      .get(`/assets/dept-${department.name}.jpg`)
-      .then((res) => {
-        setDeptImgUrl(department.name);
-      })
-      .catch((err) => {
-        setDeptImgUrl("default");
-      });
-  }, []);
+  let [imgSrc, setImgSrc] = useState(`${IMG_URL_BASE}${department.name}.jpg`);
 
   return (
-    <>
-      {deptImgUrl && (
-        <Card
+    <div className="card">
+      <Header imgSrc={imgSrc} setImgSrc={setImgSrc} />
+      <div className="card__content">
+        <Body department={department} />
+        <Buttons
           department={department}
           setSelectedDepartment={setSelectedDepartment}
-          deptImgUrl={deptImgUrl}
         />
-      )}
-    </>
+      </div>
+    </div>
   );
 };
 
-function Card({ department, setSelectedDepartment, deptImgUrl }) {
+function Header({ imgSrc, setImgSrc }) {
   return (
-    <div className="card">
-      <div className="card__image">
-        <img src={`/assets/dept-${deptImgUrl}.jpg`} alt="" />
-      </div>
-      <div className="card__content">
-        <div>
-          <h3 className="card__title">{department.name}</h3>
-          <p>{department.intro}</p>
-        </div>
-        <div className="card__buttons">
-          <button>Learn more</button>
-          <button
-            onClick={() => {
-              setSelectedDepartment({ ...department });
-            }}
-          >
-            Book Appointment
-          </button>
-        </div>
-      </div>
+    <div className="card__image">
+      <img
+        src={imgSrc}
+        onError={() => setImgSrc(`${IMG_URL_BASE}default.jpg`)}
+      />
+    </div>
+  );
+}
+
+function Body({ department }) {
+  return (
+    <div>
+      <h3 className="card__title">{department.name}</h3>
+      <p>{department.description}</p>
+    </div>
+  );
+}
+
+function Buttons(department, setSelectedDepartment) {
+  return (
+    <div className="card__buttons">
+      <button>Learn more</button>
+      <button
+        onClick={() => {
+          setSelectedDepartment({ ...department });
+        }}
+      >
+        Book Appointment
+      </button>
     </div>
   );
 }
