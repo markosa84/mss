bf86257a8d05e1ea3134e273028c556bae04774e
Lenovo <inkeko@gmail.com>
2023-11-25 14:29:44 +0100

    repair DoctorForAreaOfExpertise

--- src/main/java/hu/ak_akademia/mss/controller/DotorForAreaOfExperitceController.java
+++ src/main/java/hu/ak_akademia/mss/controller/DotorForAreaOfExperitceController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/doctors")
+@RequestMapping("/doctors")
 public class DotorForAreaOfExperitceController {
 
     @Autowired
